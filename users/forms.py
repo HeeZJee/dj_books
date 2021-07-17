d6ab507eaa3eca3218d873ba193b9663315f398c
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-07-17 18:47:53 +0500

    added sigup functionality

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.db import models
 
 CustomUser = get_user_model()
 
 class CustomUserCreationForm(UserChangeForm):
     class Meta:
-        models = CustomUser
+        model = CustomUser
         fields = ('email', 'username')
         
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        models = CustomUser
+        model = CustomUser
         fields = ('email', 'username')
